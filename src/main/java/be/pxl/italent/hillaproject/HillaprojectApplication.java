b3e18aa7f0c0b12e6749e564fa8fca7b5f6735aa
11601094 <enkhjargal.mijid@student.pxl.be>
2022-05-12 06:14:33 +0200

    Initial commit

---
+++ src/main/java/be/pxl/italent/hillaproject/HillaprojectApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.italent.hillaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HillaprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/pxl/italent/hillaproject/HillaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.italent.hillaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillaprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
